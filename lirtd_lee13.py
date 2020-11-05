4c72ef896c4c903f4c43c1d78be02d6996ec4cde
Patrick Drew <patrickmdrew@gmail.com>
2020-11-05 16:00:29 -0600

    added ability to save file

--- lirtd_lee13.py
+++ lirtd_lee13.py
@@ -10,9 +10,15 @@ from matplotlib import ticker
 plot lee+13 galaxies on the lirtd correlation from the iras sample
 """
 
+write_file = False
+write_file_plot = '../plots/raw_lirtd_lee13.pdf'
+
 # load fits
 read_file_data = '../data/lee13_fit_best_fit_params_added.pkl'
 fitF = pd.read_pickle(read_file_data)
+
+# want to limit to only believable redshifts
+# fitF = fitF.loc[fitF.z < 2.0]
 
 # load correlation params for iras
 read_file_corr_drew = 'lir_lpeak_corr_best_fit_drew.csv'
@@ -23,15 +29,21 @@ x = np.logspace(8, 14)
 # calculate lirtd correlation lpeaks
 dy = ah.lpeak_mmpz(x, dF.lam0.values, dF.eta.values)
 
+fig, ax = plt.subplots()
 plt.plot(x, dy, label='IRAS best fit')
 plt.xscale('log')
 plt.yscale('log')
+plt.title('Lee+13 galaxies with z_COSMOS2020', fontsize=12)
 plt.gca().yaxis.set_major_formatter(ticker.ScalarFormatter())
 plt.gca().yaxis.set_minor_formatter(ticker.ScalarFormatter())
-plt.scatter(10**fitF.measuredLIR, fitF.measuredLPeak, marker='.', color='k', alpha=0.7, label='lee+13')
+scat = plt.scatter(10**fitF.measuredLIR, fitF.measuredLPeak, c=fitF.z, marker='.', alpha=0.7) # color='k', 
+cb = fig.colorbar(scat, ax=ax)
+cb.set_label('z')
 plt.fill_between(x, 10**(np.log10(dy) - dF.gauss_width.values), 10**(np.log10(dy) + dF.gauss_width.values), color=sns.color_palette('mako')[1], alpha=0.3, label=r'$\pm$1$\sigma$ IRAS')
 plt.fill_between(x, 10**(np.log10(dy) - 2*dF.gauss_width.values), 10**(np.log10(dy) + 2*dF.gauss_width.values), color=sns.color_palette('mako')[1], alpha=0.1, label=r'$\pm$2$\sigma$ IRAS')
 plt.xlabel('LIR')
 plt.ylabel(r'$\lambda_{peak}$')
 plt.legend(fontsize='x-small')
+if write_file is True:
+    plt.savefig(write_file_plot, dpi=300)
 plt.show()
